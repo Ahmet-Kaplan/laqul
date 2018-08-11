1baf1a003defdd8b70725d848abedd2d5c6f266c
Ahmet Kaplan <ahmet.kaplan@uyumsoft.com.tr>
2018-08-11 13:46:33 +0300

    Added Quasar Play Showcase

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,13 +1,16 @@
 
 import authenticationPages from './authentication'
 import dashboardPages from './dashboard'
+import categories from './assets/categories'
+import LayoutShowcase from './layouts/showcase'
 
-export default [
+const routes = [
   {
     path: '/',
     caseSensitive: true,
     pathToRegexOptions: { strict: true, sensitive: true },
     component: () => import('layouts/Authentication'),
+    // component: () => import('pages/landing'),
     meta: { requiresAuth: false },
     children: authenticationPages
   },
@@ -19,11 +22,83 @@ export default [
     component: () => import('layouts/Dashboard'),
     meta: { requiresAuth: true },
     children: dashboardPages
-  },
+  }
+]
 
+function lazyLoad (path, meta) {
+  return {
+    path,
+    // caseSensitive: true,
+    // pathToRegexOptions: { strict: true, sensitive: true },
+    meta: { requiresAuth: true },
+    component: () => import('pages/showcase/' + path)
+  }
+}
+
+const showcase = {
+  path: '/showcase',
+  component: LayoutShowcase,
+  children: [
+    {
+      path: '',
+      meta: {
+        title: 'Quasar Showcase',
+        hash: '/showcase',
+        icon: 'layers',
+        backRoute: '/'
+      },
+      component: () => import('pages/showcase/index')
+    }
+  ]
+}
+
+categories.forEach(category => {
+  if (category.extract) {
+    return
+  }
+  category.features.forEach(feature => {
+    let path = category.hash + '/' + feature.hash
+
+    if (!feature.tabs) {
+      showcase.children.push(lazyLoad(path, feature))
+      return
+    }
+
+    feature.tabs.forEach(tab => {
+      let subpath = path + '/' + tab.hash
+      showcase.children.push(lazyLoad(subpath, {
+        title: tab.title,
+        hash: '/' + path,
+        iframeTabs: feature.iframeTabs,
+        icon: feature.icon,
+        tabs: feature.tabs
+      }))
+    })
+
+    routes.push({
+      path: '/showcase/' + path,
+      redirect: '/showcase/' + path + '/' + feature.tabs[0].hash
+    })
+  })
+})
+
+routes.push(showcase)
+routes.push({
+  path: '/showcase/layout-demo',
+  component: () => import('layouts/layout-demo'),
+  children: [
+    {path: 'play-with-layout', component: () => import('pages/layout-demo/play-with-layout')},
+    {path: 'drawer-panels', component: () => import('pages/layout-demo/drawer-panels')},
+    {path: 'page-sticky', component: () => import('pages/layout-demo/page-sticky')},
+    {path: 'floating-action-button', component: () => import('pages/layout-demo/floating-action-button')}
+  ]
+})
+routes.push(
   { // Always leave this as last one
     name: 'pages.errors.e404',
     path: '*',
     component: () => import('pages/404')
   }
-]
+)
+
+export default routes
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,14 +3,29 @@ import Vuex from 'vuex'
 
 import app from './app'
 import session from './session'
+import showcase from './showcase'
+import layoutDemo from './layoutDemo'
 
 Vue.use(Vuex)
 
-const store = new Vuex.Store({
+const Store = new Vuex.Store({
   modules: {
     app,
-    session
+    session,
+    showcase,
+    layoutDemo
   }
 })
 
-export default store
+if (process.env.DEV && module.hot) {
+  module.hot.accept(['./showcase'], () => {
+    const newShowcase = require('./showcase').default
+    Store.hotUpdate({ modules: { showcase: newShowcase } })
+  })
+  module.hot.accept(['./layoutDemo'], () => {
+    const newLayoutDemo = require('./layoutDemo').default
+    Store.hotUpdate({ modules: { layoutDemo: newLayoutDemo } })
+  })
+}
+
+export default Store
